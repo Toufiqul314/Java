4e8110b87ce2a41fc07815af53ad66a778a1d6d7
Toufiqul314 <49118001+Toufiqul314@users.noreply.github.com>
2023-03-01 22:24:12 +0600

    Add files via upload

---
+++ bubblesort_O_n.java
@@ -0,0 +1,41 @@
+package sorting;
+
+import java.io.*;
+import java.util.*;
+
+public class bubblesort_O_n {
+
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter array size :");
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+        boolean swaped;
+        for (int i = 0; i < arr.length - 1; i++) {
+            swaped = false;
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swaped = true;
+                }
+            }
+            if (swaped == false) {
+                break;
+            }
+        }
+        printArray(arr);
+    }
+
+}
